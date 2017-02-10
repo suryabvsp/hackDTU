4f3a9ddaa7166de4c5ecedb78f46b5bfa8023885
Surya Prakash <suryabvsp@gmail.com>
2017-02-11 05:23:22 +0530

    update

--- sonar.py
+++ sonar.py
@@ -21,49 +21,51 @@ GPIO.setup(ECHO2,GPIO.IN)
 GPIO.setup(M1,GPIO.OUT)
 GPIO.setup(M2,GPIO.OUT)
 
-def calculateDistance(TRIG, ECHO):
-	GPIO.output(TRIG,False) #waiting for sensor to settle
-	time.sleep(2)
+while 1:
 
-	GPIO.output(TRIG,True)
-	time.sleep(0.00001)
-	GPIO.output(TRIG,False) #trigger a 10us pulse
+	def calculateDistance(TRIG, ECHO):
+		GPIO.output(TRIG,False) #waiting for sensor to settle
+		time.sleep(2)
 
-	while GPIO.input(ECHO)==0:   #note time when ECHO is last LOW
-	#i.e. time at which last pulse leaves
-		pulse_start = time.time()
+		GPIO.output(TRIG,True)
+		time.sleep(0.00001)
+		GPIO.output(TRIG,False) #trigger a 10us pulse
 
-	while GPIO.input(ECHO)==1:   #note time when ECHO is last HIGH
-	#i.e. time at which last pulse arrives
-		pulse_end = time.time()
+		while GPIO.input(ECHO)==0:   #time when ECHO is last LOW
+		#i.e. time at which last pulse leaves
+			pulse_start = time.time()
 
-	#time taken for the last pulse to travel the distance
-	pulse_duration = pulse_end - pulse_start
+		while GPIO.input(ECHO)==1:  #time when ECHO is last HIGH
+		#i.e. time at which last pulse arrives
+			pulse_end = time.time()
 
-	distance = pulse_duration * 17150 #sound speed 343m/s
-	distance = round(distance,2)
-	#distance in cm
+		#time taken for the last pulse to travel the distance
+		pulse_duration = pulse_end - pulse_start
 
-	GPIO.cleanup() #reset GPIO pins
-	return distance;
+		distance = pulse_duration * 17150 #sound speed 343m/s
+		distance = round(distance,2)
+		#distance in cm
+
+		return distance;
 	
-distance1 = calculateDistance(TRIG1,ECHO1)
-distance2 = calculateDistance(TRIG2,ECHO2)
-diff = abs(distance1-distance2)
+	distance1 = calculateDistance(TRIG1,ECHO1)
+	distance2 = calculateDistance(TRIG2,ECHO2)
+	diff = abs(distance1-distance2)
 
-if distance1 > 300 and distance2 > 300:
-	GPIO.output(M1,False)
-	GPIO.output(M2,False)
-else:
-	if diff < 30:
-		GPIO.output(M1,True)
-		GPIO.output(M2,True)
+	if distance1 > 300 and distance2 > 300:
+		GPIO.output(M1,False)
+		GPIO.output(M2,False)
 	else:
-		if distance1 < distance2:
+		if diff < 30:
 			GPIO.output(M1,True)
-			GPIO.output(M2,False)
-		if distance2 < distance1:
 			GPIO.output(M2,True)
-			GPIO.output(M1,False)
-	
-	
+		else:
+			if distance1 < distance2:
+				GPIO.output(M1,True)
+				GPIO.output(M2,False)
+			if distance2 < distance1:
+				GPIO.output(M2,True)
+				GPIO.output(M1,False)
+
+	# GPIO.cleanup() #reset GPIO pins
+	time.sleep(0.3)		
